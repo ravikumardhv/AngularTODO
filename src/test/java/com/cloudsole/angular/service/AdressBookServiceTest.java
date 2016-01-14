9b729e33cdd41b002b037b7b1ee12eb781ed673a
ravikumar <java4u1983@gmail.com>
2016-01-14 11:56:28 -0700

    my first test case

---
+++ src/test/java/com/cloudsole/angular/service/AdressBookServiceTest.java
@@ -0,0 +1,26 @@
+package com.cloudsole.angular.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.cloudsole.angular.model.AddressBook;
+
+public class AdressBookServiceTest {
+	
+	AddressBookServiceImpl impl=new AddressBookServiceImpl();
+	
+	@Test
+	public void testCreateAddressBookWithNoRecords(){
+		 AddressBook addressBook1 = new AddressBook();
+	        addressBook1.setEmail("test@mail.com");
+	        addressBook1.setFirstName("John");
+	        addressBook1.setLastName("Doe");
+	        addressBook1.setPhone("12345678");
+		impl.createAddressBook(addressBook1);
+		
+		Assert.assertNull(impl.addressBooks);
+		Assert.assertTrue(impl.addressBooks.size()==1);
+		
+	}
+
+}
